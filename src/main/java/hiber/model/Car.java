f86cd4198faf416093965c2604de3692e14d5ed3
romaRacoon <roma1808@inbox.ru>
2022-07-04 01:15:03 +0300

    task is done

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,42 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Table(name = "cars")
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    public Car(){}
+
+    public Car(String model, int series){
+        this.model = model;
+        this.series = series;
+    }
+
+
+    public int getSeries() {
+        return series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
